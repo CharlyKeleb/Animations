ab40ecef6d61a89bf2db766626a6c61ca41bb067
CharlyKeleb <chizaramsuccess9@gmail.com>
2022-07-08 10:19:40 +0100

    added starbucks coffee cup

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sizer/sizer.dart';
-import 'package:test_project/androidlogo/home.dart';
-import 'package:test_project/androidlogo/splash.dart';
-
-import 'package:test_project/battery/home.dart';
-import 'package:test_project/chess/chess.dart';
-import 'package:test_project/chess/home.dart';
-import 'package:test_project/cup/cup.dart';
-import 'package:test_project/card_flip/flip_animation.dart';
-import 'package:test_project/squidgame/home.dart';
 import 'package:test_project/starbucks/home.dart';
-import 'package:test_project/web_ui/pepsi/pepsi.dart';
 
 void main() {
   runApp(const MyApp());
